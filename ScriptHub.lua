42c4f0c6410a907cb5f0323c70fc97cbc289aae0
SkyLi000 <97918490+SkyLi000@users.noreply.github.com>
2022-04-01 17:11:25 +1300

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -13,14 +13,35 @@ function Notify(Title, Message, Duration)
 end
 
 if game.PlaceId == 142823291 then
-  Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Murder%20Mystery%202")
-  Notify("Loaded Successfully ✅", "Murder Mystery Script Loaded Successfully!", 10)
+  if _G.ScriptHubEnabled == true then
+    if _G.MurderMystery2Loaded == nil then
+      _G.MurderMystery2Loaded = true
+      Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Murder%20Mystery%202")
+      Notify("Loaded Successfully ✅", "Murder Mystery Script Loaded Successfully!", 10)
+    else
+      Notify("Error", "Murder Mystery 2 Script Has Already Been Loaded!", 10)
+     end
+   end
 elseif game.PlaceId == 3956818381 then
-  Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Ninja%20Legends")
-  Notify("Loaded Successfully ✅", "Ninja Legends Script Loaded Successfully!", 10)
+  if _G.ScriptHubEnabled == true then
+    if _G.NinjaLegendsLoaded == nil then
+      _G.NinjaLegendsLoaded = true
+      Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Ninja%20Legends")
+      Notify("Loaded Successfully ✅", "Ninja Legends Script Loaded Successfully!", 10)
+    else
+      Notify("Error", "Ninja Legends Script Has Already Been Loaded!", 10)
+    end
+  end
 elseif game.PlaceId == 6284583030 then
-  Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Pet%20Simulator%20X.lua")
-  Notify("Loaded Successfully ✅", "Pet Simulator X Script Loaded Successfully!", 10)
+  if _G.ScriptHubEnabled == true then
+    if _G.PetSimXLoaded == nil then
+      _G.PetSimXLoaded = true
+      Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Pet%20Simulator%20X.lua")
+      Notify("Loaded Successfully ✅", "Pet Simulator X Script Loaded Successfully!", 10)
+    else
+      Notify("Error", "Pet Simulator X Script Has Already Been Loaded!", 10)
+    end
+  end
 else
   Notify("No Script Found ❌", "Sorry No Script Found For This Game.")
 end
