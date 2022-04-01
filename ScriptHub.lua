330d92fcb662dc56f133452c530d31f5a4be879d
SkyLi000 <97918490+SkyLi000@users.noreply.github.com>
2022-04-01 16:39:39 +1300

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -14,10 +14,13 @@ end
 
 if game.PlaceId == 142823291 then
   Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Murder%20Mystery%202")
-  Notify("Loaded Successfully ✅", "Murder Mystery Script Loaded Successfully!", 5)
+  Notify("Loaded Successfully ✅", "Murder Mystery Script Loaded Successfully!", 10)
 elseif game.PlaceId == 3956818381 then
   Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Ninja%20Legends")
-  Notify("Loaded Successfully ✅", "Ninja Legends Script Loaded Successfully!", 5)
+  Notify("Loaded Successfully ✅", "Ninja Legends Script Loaded Successfully!", 10)
+elseif game.PlaceId == 6284583030 then
+  Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Pet%20Simulator%20X.lua")
+  Notify("Loaded Successfully ✅", "Pet Simulator X Script Loaded Successfully!", 10)
 else
   Notify("No Script Found ❌", "Sorry No Script Found For This Game.")
 end
