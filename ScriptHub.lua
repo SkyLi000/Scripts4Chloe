bcb7aaad7f73a67dcff0917a975342f2dc550617
SkyLi000 <97918490+SkyLi000@users.noreply.github.com>
2022-04-01 16:10:05 +1300

    Create ScriptHub.lua

---
+++ ScriptHub.lua
@@ -0,0 +1,23 @@
+function Load(URL)
+  loadstring(game:HttpGet(URL))()
+end
+
+function Notify(Title, Message, Duration)
+  local CoreGui = game:GetService("StarterGui")
+
+  CoreGui:SetCore("SendNotification", {
+    Title = Title;
+    Text = Message;
+    Duration = Duration;
+  })
+end
+
+if game.PlaceId == 142823291 then
+  Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Murder%20Mystery%202")
+  Notify("Loaded Successfully ✅", "Murder Mystery Script Loaded Successfully!", 5)
+elseif game.PlaceId == 3956818381 then
+  Load("https://raw.githubusercontent.com/SkyLi000/Scripts4Chloe/main/Ninja%20Legends")
+  Notify("Loaded Successfully ✅", "Ninja Legends Script Loaded Successfully!", 5)
+else
+  Notify("No Script Found ❌", "Sorry No Script Found For This Game.")
+end
